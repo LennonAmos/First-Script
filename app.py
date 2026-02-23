3e064f40761d8f37e22e05ae830e205ed790aee6
LennonAmos <amoslennon7@gmail.com>
2026-02-23 10:54:19 +0000

    Release

---
+++ app.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+age = int(input("what is your age?"))
+print("Welcome", name, "you are", age)
+if age >=13 :
+    print("You are a Teenager")
+else:
+    print("You are a child")
